93c154c7e7b08654b9062e87bca4c01207a8cac8
Vicente Garcia Andrade <vicgarci@c2r3s5.42madrid.com>
2025-02-10 18:53:29 +0100

    Problemas con la base de datos

--- Register/code/configs/settings.py
+++ Register/code/configs/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'configs.urls'
+ROOT_URLCONF = 'app.code.configs.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'configs.wsgi.application'
+WSGI_APPLICATION = 'app.code.configs.wsgi.application'
 
 
 # Database
--- Register/code/configs/urls.py
+++ Register/code/configs/urls.py
@@ -24,7 +24,7 @@ from    django.conf import settings
 urlpatterns = [
 	path("", TemplateView.as_view(template_name="home.html"), name="home"),
 	path('admin/', admin.site.urls),
-	path("accounts/", include("accounts.urls")),
+	path("accounts/", include("app.code.accounts.urls")),
 	path("accounts/", include("django.contrib.auth.urls")),
 ]
 
