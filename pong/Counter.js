17228367e80c7de34ad14333ecaa2d1a2aa6df34
witemirlo <jacoboberdugo@gmail.com>
2025-02-11 15:42:13 +0100

    now the counter has a black line for visibility

--- pong/Counter.js
+++ pong/Counter.js
@@ -16,11 +16,15 @@ constructor(obj, canvas, context) {
 render() {
 	this.context.font = this.font;// TODO: quizas hacer esto en bucle es innecesario
 	this.context.fillStyle = this.color;
+	this.context.strokeStyle = "black"; // TODO: harcoded
+	this.context.lineWidth = "1";
 
 	this.context.textAlign = "end";
+	this.context.strokeText(`${this.player1_score}`, this.canvas.width / 4, this.canvas.height / 8);
 	this.context.fillText(`${this.player1_score}`, this.canvas.width / 4, this.canvas.height / 8);
 
 	this.context.textAlign = "start";
+	this.context.strokeText(`${this.player2_score}`, (this.canvas.width / 4) * 3, this.canvas.height / 8);
 	this.context.fillText(`${this.player2_score}`, (this.canvas.width / 4) * 3, this.canvas.height / 8);
 }
 
