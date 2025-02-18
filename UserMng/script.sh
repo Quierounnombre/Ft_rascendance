3d2593d25ad0977bd2aeda06716ee5ba4287ff81
Vicente Garcia Andrade <vicgarci@c1r9s4.42madrid.com>
2025-02-18 15:43:01 +0100

    Superuser creator

--- UserMng/script.sh
+++ UserMng/script.sh
@@ -2,6 +2,7 @@
 
 python3 /UserMng/code/manage.py	makemigrations
 python3 /UserMng/code/manage.py	migrate
+python3 /UserMng/code/manage.py createsuperuser --username ${SUPER_USER} --email ${SUPER_EMAIL} --database default --no-input
 
 #gunicorn --bind 0.0.0.0:8000 --workers 3 Register.code.configs.wsgi:application
 exec python3 /UserMng/code/manage.py runserver 0.0.0.0:${BACKEND_PORT}
