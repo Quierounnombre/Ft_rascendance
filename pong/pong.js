5690631e62e7ae477a30c1a17dd080c35951b42a
witemirlo <jacoboberdugo@gmail.com>
2025-02-27 16:59:13 +0100

    working in generate a game from a form

--- pong/pong.js
+++ pong/pong.js
@@ -1,10 +1,11 @@
 import { Game } from "./Game.js";
 
-function main() {
-	const tmp = JSON.parse(`[{"type":"config","timeout":30000,"max_score":3},{"id":"player1","type":"player","x":10,"y":200,"width":20,"height":100,"speed":2,"move_up":"w","move_down":"s"},{"id":"player2","type":"player","x":790,"y":200,"width":20,"height":100,"speed":2,"move_up":"ArrowUp","move_down":"ArrowDown"},{"id":"ball","type":"ball","x":400,"y":200,"dirX":1,"dirY":1,"radius":10},{"id":"counter","type":"counter","x":400,"y":10,"font":"42px Arial"}]`);
-	const game = new Game(tmp);
+export default function pong(config) {
+	// const tmp = JSON.parse(`[{"type":"config","timeout":30000,"max_score":3},{"id":"player1","type":"player","x":10,"y":200,"width":20,"height":100,"speed":2,"move_up":"w","move_down":"s"},{"id":"player2","type":"player","x":790,"y":200,"width":20,"height":100,"speed":2,"move_up":"ArrowUp","move_down":"ArrowDown"},{"id":"ball","type":"ball","x":400,"y":200,"dirX":1,"dirY":1,"radius":10},{"id":"counter","type":"counter","x":400,"y":10,"font":"42px Arial"}]`);
+	const game = new Game(JSON.parse(config));
+	// const game = new Game(tmp);
 	game.gameLoop();
 	// TODO: exportar info de la partida
 }
 
-main();
+pong();
