0c3db148366f214958e7d65cd42dc80e5e51f25c
witemirlo <jacoboberdugo@gmail.com>
2025-03-12 11:06:09 +0100

    player object in python

---
+++ websocket/code/pong/Player.py
@@ -0,0 +1,16 @@
+from CanvasObject import CanvasObject
+
+class Player(CanvasObject):
+    def __init__(self, obj):
+        super().__init__(obj)
+    
+    def slide(self, dirX, dirY):
+        self.y += self.speed * dirY
+        if (!self.is_moving) {
+            if (self.dirY > 0)
+                self.dirY -= 1
+            else if (self.dirY < 0)
+                self.dirY += 1
+        }
+        self.recalculateHitbox()
+        self.keepInsideCanvas()
