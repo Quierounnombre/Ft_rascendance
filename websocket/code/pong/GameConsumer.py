6a6ce303995011b5388b6508e13395f77d02daab
witemirlo <jacoboberdugo@gmail.com>
2025-03-20 12:06:23 +0100

    working in channels workers

--- websocket/code/pong/GameConsumer.py
+++ websocket/code/pong/GameConsumer.py
@@ -6,31 +6,45 @@ from asgiref.sync import async_to_sync
 
 from Game import Game
 
+game_rooms = {}
+
 class GameConsumer(SyncConsumer):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.room_name = f"pong_{self.scope["url_route"]["kwargs"]["room_name"]}"
 
-    def start(self) -> None:
-        self.game.start()
+    # message: {"room_id: string"}
+    def game_start(self, event) -> None:
+        game_rooms[event["message"]["room_id"]].start()
     
-    # message: {"payerN: int"}
+    # message: {
+    #     "room_id": str,
+    #     "payerN": int
+    # }
     def player_direction(self, event) -> None:
         message = event["message"]
         player_id = message.keys()[0]
 
-        self.game.setPlayerDir(player_id, message[player_id])
+        game_rooms[message["room_id"]].setPlayerDir(player_id, message[player_id]) # TODO: seguro que esto funciona?
     
-    # "message": {"playerN": int}
+    # message: {
+    #     "room_id": str,
+    #     "payerN": int
+    # }
     def set_player(self, event) -> None:
         message = event["message"]
         player_id = message.keys()[0]
 
-        for obj in self.game.game_objects:
+        for obj in game_rooms[message["room_id"]].game_objects:
             if obj.type == player_id:
                 obj.id = message["player_id"]
     
-    # "message": json converted to string with the config
+    # message: {
+    #     "room_id": str,
+    #     "data": json converted to string with the config
+    # }
     def game_config(self, event) -> None:
-        self.game = Game(event["message"])
+        message = event["message"]
+
+        game_rooms[message["room_id"]] = Game(message["data"])
