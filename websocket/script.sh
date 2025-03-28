0fbd3810fc566989d8e72caa98a2758e9faee77f
witemirlo <jacoboberdugo@gmail.com>
2025-03-28 16:31:19 +0100

    preparing for merge

---
+++ websocket/script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python3 /websocket/code/manage.py makemigrations
+python3 /websocket/code/manage.py migrate
+
+exec python3 /websocket/code/manage.py runworker game_engine &>/var/log/server.log &
+exec python3 /websocket/code/manage.py runserver 0.0.0.0:${WEBSOCKET_PORT}
