267b08ba68b439cc2a67d99d66c4c6371fb66183
pablo-is-a-goblin <pablo.sacristan.pernia@gmail.com>
2025-04-01 17:59:00 +0200

    deleted mockup

--- nginx-files/static-files/loadHistory.js
+++ nginx-files/static-files/loadHistory.js
@@ -8,52 +8,10 @@ export default function loadHistory() {
 		window.location.hash = "";
 		return ;
 	}
-	root.replaceChildren(mockUpHistory());
 
 	getUser(token).then((user) => {
 		getHistoryTable(user, token).then((table) => {
 			// root.replaceChildren(table);
 	})});
 
-}
-
-function mockUpHistory() {
-	const form = document.createElement("form");
-	form.innerHTML = `
-  <div class="mb-3">
-    <label for="winner" class="form-label">Winner id:</label>
-    <input type="number" class="form-control" id="winner" name="winner_id">
-  </div>
-  <div class="mb-3">
-    <label for="winner_goals" class="form-label">Winner goals:</label>
-    <input type="number" class="form-control" id="winner_goals" name="winner_goals">
-  </div>
- <div class="mb-3">
-    <label for="loser" class="form-label">loser id:</label>
-    <input type="number" class="form-control" id="loser" name="loser_id">
-  </div>
-  <div class="mb-3">
-    <label for="loser_goals" class="form-label">loser goals:</label>
-    <input type="number" class="form-control" id="loser_goals" name="loser_goals">
-  </div>
- <div class="mb-3">
-    <label for="duration" class="form-label">duration</label>
-    <input type="text" class="form-control" id="duration" name="duration">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>`
-
-  form.addEventListener("submit", (event) => {
-	event.preventDefault();
-	sendMatch(form);
-})
-
-	return form;
-}
-
-function sendMatch(form) {
-	const data = new FormData(form);
-	fetch("https://" + window.location.hostname + ":7000/history/add/", {
-		method: "POST",
-		body: data,
-	});
 }
