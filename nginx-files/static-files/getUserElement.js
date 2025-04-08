f3e99215268523973eb743d8c15cac533f88e1d2
ferri666 <fft006@gmail.com>
2025-04-08 12:55:16 +0200

    Font only accepts numbers (and capped)

--- nginx-files/static-files/getUserElement.js
+++ nginx-files/static-files/getUserElement.js
@@ -6,7 +6,11 @@ export default function getUserElement(user) {
 		<input type="text" readonly class="form-control-plaintext" id="email" name="email" value="`+ user["email"] +`">`;
 
 	const usernameField = emailField.cloneNode(true);
-	const fontField = emailField.cloneNode(true);
+	const fontField = document.createElement("div");
+	fontField.setAttribute("class", "mb-3 row");
+	fontField.innerHTML = `<label for="" data-i18n-key="prof-font" class="col-sm-2 form-label col-form-label">Font Size: </label>
+		<div class="col-sm-10">
+		<input type="number" readonly class="form-control-plaintext" id="font" name="font" min="11" max="33" value="`+ user["font"] +`">`;
 	const languageField = document.createElement("div");
 	languageField.setAttribute("class", "mb-3 row");
 	languageField.innerHTML = `<label for="language" data-i18n-key="prof-lang" class="col-sm-2 form-label col-form-label">Language: </label>
@@ -18,22 +22,16 @@ export default function getUserElement(user) {
         </select>`;
 
 	usernameField.getElementsByTagName("label")[0].setAttribute('for', "username");
-	fontField.getElementsByTagName("label")[0].setAttribute('for', "font");
 
     usernameField.getElementsByTagName("label")[0].setAttribute('data-i18n-key', "prof-uname");
-	fontField.getElementsByTagName("label")[0].setAttribute('data-i18n-key', "prof-font");;
 
 	usernameField.getElementsByTagName("label")[0].innerHTML="Username: ";
-	fontField.getElementsByTagName("label")[0].innerHTML="Font size:";
 
 	usernameField.getElementsByTagName("input")[0].setAttribute('id', "username");
-	fontField.getElementsByTagName("input")[0].setAttribute('id', "font");
 
 	usernameField.getElementsByTagName("input")[0].setAttribute('name', "username");
-	fontField.getElementsByTagName("input")[0].setAttribute('name', "font");
-	
+
 	usernameField.getElementsByTagName("input")[0].setAttribute('value', user["username"]);
-	fontField.getElementsByTagName("input")[0].setAttribute('value', user["font"]);
     if (user["language"]==="ENG")
 	languageField.getElementsByTagName("option")[0].setAttribute('selected', "");
     if (user["language"]==="ESP")
