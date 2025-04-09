bac1ae979d9e99900fd2d474054fde71a8dd4f9b
witemirlo <jacoboberdugo@gmail.com>
2025-04-09 14:36:53 +0200

    add TournamentConsumer to routing

--- websocket/code/pong/routing.py
+++ websocket/code/pong/routing.py
@@ -1,8 +1,9 @@
 # chat/routing.py
 from django.urls import re_path
 
-from . import PongConsumer
+from . import PongConsumer, TournamentConsumer
 
 websocket_urlpatterns_pong = [
     re_path(r"ws/pong/(?P<room_name>\w+)/$", PongConsumer.PongConsumer.as_asgi()),
+    re_path(r"ws/tournament/(?P<room_name>\w+)/$", TournamentConsumer.TournamentConsumer.as_asgi()),
 ]
