fc514b6f5caacd2b4731ea24ec451073764e05ad
ferri666 <fft006@gmail.com>
2025-04-12 17:46:55 +0200

    Just a bit bigger

--- nginx-files/static-files/loadModeGame.js
+++ nginx-files/static-files/loadModeGame.js
@@ -7,19 +7,19 @@ gameSelector.innerHTML = `
         <div class="col border border-5 border-info-subtle rounded-pill p-4 text-center me-3" style="background-color: lightskyblue; padding:30px">
         <a class="link-offset-2 link-underline link-underline-opacity-0" href="#form-tourn">
         <i class="bi bi-trophy-fill" style="font-size:12rem; color:blue"></i>
-        <p data-i18n-key="mod-tourn" style="color: blue; font-size:1.6rem;">Tournament</p>
+        <p data-i18n-key="mod-tourn" style="color: blue; font-size:2rem;">Tournament</p>
         </a>
         </div>
         <div class="col border border-5 border-info-subtle rounded-pill p-4 text-center me-3" style="background-color: lightskyblue; padding:33px">
         <a class="link-offset-2 link-underline link-underline-opacity-0" href="#form-online">
         <i class="bi bi-globe2" style="font-size:12rem; color:blue"></i>
-        <p style="color: blue; font-size:1.6rem;">Online</p>
+        <p style="color: blue; font-size:2rem;">Online</p>
         </a>
         </div>
         <div class="col border border-5 border-info-subtle rounded-pill p-4 text-center" style="background-color: lightskyblue; padding:33px">
         <a class="link-offset-2 link-underline link-underline-opacity-0" href="#form-local">
         <i class="bi bi-house-fill" style="font-size:12rem; color:blue"></i>
-        <p style="color: blue; font-size:1.6rem;">Local</p>
+        <p style="color: blue; font-size:2em;">Local</p>
         </a>
         </div>
     </div>
