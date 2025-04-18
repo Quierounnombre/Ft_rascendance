675b68f96feb141abaf2a5d33232b6655a8201a9
Vicente Garcia Andrade <vicgarci@c2r11s6.42madrid.com>
2025-04-18 12:44:54 +0200

    Listo twofactorauth secret key

--- TwoFactorAuth/code/configs/settings.py
+++ TwoFactorAuth/code/configs/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gtf9cdg8r^b5hq38viid5v1s51jl9x4-2i0zv7k2j$9l)rydmz'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
